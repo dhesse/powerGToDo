903e325e66ae8ada1c1fb560e03b0ac92b3d2fd1
Dirk Hesse <dirk.hesse@posteo.net>
2023-07-12 15:42:21 +0200

    Adding next action, projects, waiting lists.

--- powerGToDo.py
+++ powerGToDo.py
@@ -9,32 +9,37 @@ import pickle
 import itertools
 import sys
 
+
 def get_config():
     return json.load(open("config.json"))
 
+
 class Task:
 
-    def __init__(self, parent: "TodoList", title: str, id: str, createdDateTime: object, **args):
-        
+    def __init__(self, parent: "TodoList", title: str, id: str,
+                 createdDateTime: object, **args):
+
         self.parent = parent
         self.title = title
         self.created = parse(createdDateTime)
         self.id = id
         self.args = args
-    
+
     @property
     def tags(self) -> list[str]:
         return re.findall(r"\#(\w+)", self.title)
+
 
 class TodoList:
 
     def __init__(self, task_list_gen: Callable[["TodoList"], list[Task]],
                  displayName: str, id: str, **args):
-        
+
         self.name = displayName
         self.id = id
         self.args = args
         self.tasks = task_list_gen(self)
+
 
 class AzureToDo:
 
@@ -46,50 +51,72 @@ class AzureToDo:
         self.headers = {"Authorization": config['access_token']}
         list_raw = requests.get(self.LISTS_URL, headers=self.headers)
         self.lists = [TodoList(self.get_tasks, **a)
-        for a in progressbar.progressbar(list_raw.json()['value'])]
-    
+                      for a in
+                      progressbar.progressbar(list_raw.json()['value'])]
+
     def get_tasks(self, parent: TodoList) -> list[Task]:
-        tasks_raw = requests.get(self.LISTS_URL + f"/{parent.id}/tasks", headers=self.headers)
+        tasks_raw = requests.get(self.LISTS_URL + f"/{parent.id}/tasks",
+                                 headers=self.headers)
         return [Task(parent, **t) for t in tasks_raw.json()['value']]
-    
+
     def forget_token(self) -> None:
         del self.config['access_token']
+
+    @property
+    def projects(self) -> list[TodoList]:
+        return [f for f in self.lists if f.name in
+                self.config.get('projects', [])]
+
+    @property
+    def next_action(self) -> list[TodoList]:
+        return [f for f in self.lists if f.name in
+                self.config.get('next_action', [])]
+
+    @property
+    def waiting(self) -> list[TodoList]:
+        return [f for f in self.lists if f.name in
+                self.config.get('waiting', [])]
+
 
 class CLI:
 
     def __init__(self, todo):
-        
+
         self.todo = todo
         self.actions = {'l': {'desc': "Show ToDo lists", 'a': self.show_lists},
                         's': {'desc': "Show stale tasks", 'a': self.stale},
                         'q': {'desc': "quit", 'a': self.quit},
                         'h': {'desc': "Show help mesage", 'a': self.help}}
-        
+
     def __call__(self, *args: Any, **kwds: Any) -> Any:
-        
+
         while True:
             fn = self.actions.get(
                 input("choose action (h for help, Ctrl+C to quit): "),
-                {'a': self.help})['a']()
+                {'a': self.help})
+            fn['a']()
 
     def show_lists(self) -> None:
         for n, l in enumerate(self.todo.lists):
             print(f"({n:4}) {l.name}")
-    
+
     def help(self) -> None:
         print("available commands:")
         for a in self.actions:
             print(f"{a}: {self.actions[a]['desc']}")
-    
+
     def stale(self) -> None:
         N = int(input("How many stale tasks to show (default: 20)") or 20)
-        all_tasks = [t for t in itertools.chain.from_iterable(l.tasks for l in self.todo.lists) if t.args['status'] != 'completed']
+        all_tasks = [t for t in itertools.chain.from_iterable(
+            lst.tasks for lst in self.todo.lists)
+            if t.args['status'] != 'completed']
         all_tasks.sort(key=lambda t: t.created)
         for t in all_tasks[:N]:
             print(f"{t.created} -> {t.title} | {t.parent.name}")
 
     def quit(self) -> None:
         sys.exit(0)
+
 
 if __name__ == "__main__":
 
--- test_powerGToDo.py
+++ test_powerGToDo.py
@@ -4,6 +4,7 @@ import datetime
 
 from powerGToDo import TodoList, Task, AzureToDo
 
+
 class TestToDoList(TestCase):
 
     def test_factory_is_called(self):
@@ -12,7 +13,7 @@ class TestToDoList(TestCase):
         task_list = TodoList(factory, "foo", "bar")
         factory.assert_called_with(task_list)
         self.assertListEqual(task_list.tasks, [])
-    
+
 
 class TestTask(TestCase):
 
@@ -22,19 +23,36 @@ class TestTask(TestCase):
         task = Task(parent, "foo", "bar", "2010-10-20 20:51:52")
         self.assertEqual(task.created,
                          datetime.datetime(2010, 10, 20, 20, 51, 52))
-    
+
     def test_tags(self):
 
         task = Task(MagicMock(), "This task has no tags.", "bar", "2010-10-10")
         self.assertListEqual(task.tags, [])
-        
-        task = Task(MagicMock(), "This task has #two cool #tags.", "bar", "2010-10-10")
+
+        task = Task(MagicMock(), "This task has #two cool #tags.",
+                    "bar", "2010-10-10")
         self.assertListEqual(task.tags, ["two", "tags"])
+
 
 class TestAzureToDo(TestCase):
 
     @patch("powerGToDo.requests.get")
-    @patch("powerGToDo.get_config", new=MagicMock(return_value={'access_token': 'foo123'}))
+    @patch("powerGToDo.get_config",
+           new=MagicMock(return_value={'access_token': 'foo123'}))
     def test_constructor(self, get_mock):
         todo = AzureToDo()
-        get_mock.assert_called_with(todo.LISTS_URL, headers={'Authorization': 'foo123'})
+        get_mock.assert_called_with(todo.LISTS_URL, headers={
+                                    'Authorization': 'foo123'})
+
+    @patch("powerGToDo.requests.get")
+    @patch("powerGToDo.get_config")
+    def test_porperties(self, *a):
+        todo = AzureToDo()
+        todo.config = {'projects': ['proj'],
+                       'waiting': ['wf'], 'next_action': ['na']}
+        todo.lists = [TodoList(MagicMock(retun_value=[]), 'proj', 'foo'),
+                      TodoList(MagicMock(retun_value=[]), 'wf', 'foo'),
+                      TodoList(MagicMock(retun_value=[]), 'na', 'foo')]
+        self.assertListEqual(todo.projects, [todo.lists[0]])
+        self.assertListEqual(todo.waiting, [todo.lists[1]])
+        self.assertListEqual(todo.next_action, [todo.lists[2]])
